e3b068a9cba729401d9caece210c15767220ca37
Lara Pirkner <2077910@talnet.com>
2020-04-23 11:12:45 +0200

    Uitwerking van de exercise

--- film_add.php
+++ film_add.php
@@ -1,20 +1,19 @@
 <?php
     require 'includes/connect.php';
     
-    $title=$_POST['TitleA'];
-    $duur=$_POST['DuurA'];
-    $datum=$_POST['DateA'];
-    $land=$_POST['CountryA'];
-    $description=$_POST['DescriptionA'];
-    $video=$_POST['VideoA'];
+    $title = $_POST['TitleA'];
+    $duur = $_POST['DuurA'];
+    $datum = $_POST['DateA'];
+    $land = $_POST['CountryA'];
+    $description = $_POST['DescriptionA'];
+    $video = $_POST['VideoA'];
 
-    $sql="INSERT INTO films SET Title=? , Duur=?, Datum=?, Land=?,
+    $sql = "INSERT INTO films SET Title=? , Duur=?, Datum=?, Land=?,
      Description=?, Video=? ";
-    $stnt=$pdo->prepare($sql);
+    $stnt = $pdo->prepare($sql);
     $stnt->execute([$title,$duur,$datum,
     $land,$description,$video]);
 
     echo "<body style='background-color:#CAEA84'>";
     echo "<p><a href='index.php'>Terug</a></p>" ;
-
 ?>
--- series_up.php
+++ series_up.php
@@ -1,23 +1,22 @@
 <?php
     require 'includes/connect.php';
     
-    $id=$_GET['id'];
-    $title=$_POST['Title'];
-    $rating=$_POST['Rating'];
-    $description=$_POST['Description'];
-    $awards=$_POST['Awards'];
-    $seasons=$_POST['Season'];
-    $country=$_POST['Country'];
-    $language=$_POST['Language'];
+    $id = $_GET['id'];
+    $title = $_POST['Title'];
+    $rating = $_POST['Rating'];
+    $description = $_POST['Description'];
+    $awards = $_POST['Awards'];
+    $seasons = $_POST['Season'];
+    $country = $_POST['Country'];
+    $language = $_POST['Language'];
 
 
-    $sql="UPDATE series SET Title=? , Rating=?,
-     Description=?, Awards=?, Season=?, Country=?, Language=?  where id=?";
-    $stnt=$pdo->prepare($sql);
+    $sql = "UPDATE series SET Title=? , Rating=?,
+    Description=?, Awards=?, Season=?, Country=?, Language=?  where id=?";
+    $stnt = $pdo->prepare($sql);
     $stnt->execute([$title,$rating,$description,
     $awards,$seasons,$country,$language,$id]);
 
     echo "<body style='background-color:#CAEA84'>";
     echo "<p><a href='index.php'>Terug</a></p>" ;
-
 ?>
